e0abbcdc4fd637e14529dccd474865cdeac775e5
Johnatan99 <johnatan.esb1999@gmail.com>
2024-04-20 14:53:47 -0300

    APP alterado

--- src/app/views.py
+++ src/app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+def home(request):
+    context = {
+        'title': "Página principal",
+        'content': "Bem vindo à página inicial"
+    } 
+
+    return render(request, 'index.html', context)
+
+def produtos(request):
+    return render(request, 'produtos.html')
+
+def contato(request):
+    context = {
+        'title': "Página de contato",
+        'content': "Bem vindo à página de contato"
+    }
+    return render(request, 'contato.html', context)
+
+def conta(request):
+    return render(request, 'conta.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
 # Create your views here.
