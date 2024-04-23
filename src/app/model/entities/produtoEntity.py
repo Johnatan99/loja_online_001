f717ba51920ae70f0658626270744834a163bbab
Johnatan99 <johnatan.esb1999@gmail.com>
2024-04-23 08:13:32 -0300

    Modelo de produto criado; admin e settings alterados para receber o app products

--- src/app/model/entities/produtoEntity.py
+++ src/app/model/entities/produtoEntity.py
@@ -1,9 +1,10 @@
 from typing import Dict
 class Produto:
     
-    def __init__(self, id_produto, nome_produto, foto_produto, nota, valor) -> None:
+    def __init__(self, id_produto, nome_produto, foto_produto, nota, valor, categoria) -> None:
         self.id_produto = id_produto
         self.nome_produto = nome_produto
         self.foto_produto = foto_produto
         self.nota = nota
-        self.vaor = valor
+        self.valor = valor
+        self.categoria = categoria
--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    #id_produto =
+    nome_produto = models.Charfield(max_length=120)
+    #foto_produto
+    #nota =
+    valor = models.DecimalField(decimal_places=2, max_digts=20, default=100.00)
+    #categoria
+    descricao = models.TextField()
+    
